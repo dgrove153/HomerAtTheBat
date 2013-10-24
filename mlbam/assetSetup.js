d5147444802a04ec68cbe7083ee9eead447a9382
Ari Golub <arigolub@gmail.com>
2013-10-24 15:59:21 -0400

    model change

--- mlbam/assetSetup.js
+++ mlbam/assetSetup.js
@@ -47,37 +47,37 @@ Team.find({}, function(err, docs) {
 });
 
 var lastYearTrans = [
-{ buyer : "SHAW", seller : "PUIG", transaction: { type : "MLB_DRAFT_CASH", value : 8, year : 2014 }},
-{ buyer : "PUIG", seller : "MAD", transaction: { type : "MLB_DRAFT_CASH", value : 5, year : 2014 }},
-{ buyer : "PUIG", seller : "LAZ", transaction: { type : "MLB_DRAFT_CASH", value : 8, year : 2014 }},
-{ buyer : "HIV+", seller : "SHAW", transaction: { type : "MLB_DRAFT_CASH", value : 3, year : 2014 }},
-{ buyer : "PUIG", seller : "DBAG", transaction: { type : "MLB_DRAFT_CASH", value : 3, year : 2014 }},
-{ buyer : "PUIG", seller : "MAD", transaction: { type : "MLB_DRAFT_CASH", value : 7, year : 2014 }},
-{ buyer : "DBAG", seller : "PUIG", transaction: { type : "FA_AUCTION_CASH", value : 1, year : 2014 }},
-{ buyer : "LAZ", seller : "PUIG", transaction: { type : "MLB_DRAFT_CASH", value : 1, year : 2014 }},
-{ buyer : "CHOB", seller : "PUIG", transaction: { type : "MLB_DRAFT_CASH", value : 3, year : 2014 }},
-{ buyer : "CHOB", seller : "PUIG", transaction: { type : "FA_AUCTION_CASH", value : 99, year : 2014 }},
-{ buyer : "CHOB", seller : "PUIG", transaction: { type : "MILB_DRAFT_SWAP_RIGHTS", value : 1, year : 2014 }},
-{ buyer : "SIDO", seller : "GOB", transaction: { type : "MLB_DRAFT_CASH", value : 6, year : 2014 }},
-{ buyer : "GRAN", seller : "PUIG", transaction: { type : "MLB_DRAFT_CASH", value : 10, year : 2014 }},
-{ buyer : "MAD", seller : "PUIG", transaction: { type : "MLB_DRAFT_CASH", value : 13, year : 2014 }},
-{ buyer : "PUIG", seller : "SHAW", transaction: { type : "MLB_DRAFT_CASH", value : 8, year : 2014 }},
-{ buyer : "SHAW", seller : "GLRY", transaction: { type : "MLB_DRAFT_CASH", value : 5, year : 2014 }},
-{ buyer : "HIV+", seller : "PUIG", transaction: { type : "MILB_DRAFT_SWAP_RIGHTS", value : 4, year : 2014 }},
-{ buyer : "GLRY", seller : "GOB", transaction: { type : "MLB_DRAFT_CASH", value : 2, year : 2014 }},
-{ buyer : "PUIG", seller : "GRAN", transaction: { type : "MLB_DRAFT_CASH", value : 7, year : 2014 }},
-{ buyer : "SHAW", seller : "GRAN", transaction: { type : "MLB_DRAFT_CASH", value : 3, year : 2014 }},
-{ buyer : "SHAW", seller : "PUIG", transaction: { type : "MLB_DRAFT_CASH", value : 3, year : 2014 }},
-{ buyer : "SHAW", seller : "PUIG", transaction: { type : "MLB_DRAFT_CASH", value : 3, year : 2015 }},
-{ buyer : "PUIG", seller : "MAD", transaction: { type : "MLB_DRAFT_CASH", value : 5, year : 2014 }},
-{ buyer : "GLRY", seller : "GOB", transaction: { type : "MLB_DRAFT_CASH", value : 3, year : 2014 }},
-{ buyer : "SHAW", seller : "GLRY", transaction: { type : "MLB_DRAFT_CASH", value : 5, year : 2014 }},
-{ buyer : "JEFF", seller : "MAD", transaction: { type : "MLB_DRAFT_CASH", value : 5, year : 2014 }},
-{ buyer : "PUIG", seller : "JEFF", transaction: { type : "MLB_DRAFT_CASH", value : 5, year : 2014 }},
-{ buyer : "SIDO", seller : "PUIG", transaction: { type : "MLB_DRAFT_CASH", value : 3, year : 2014 }},
-{ buyer : "CHOB", seller : "GOB", transaction: { type : "MILB_DRAFT_SWAP_RIGHTS", value : 3, year : 2014 }},
-{ buyer : "JEFF", seller : "CHOB", transaction: { type : "MLB_DRAFT_CASH", value : 6, year : 2014 }},
-{ buyer : "MAD", seller : "CHOB", transaction: { type : "MLB_DRAFT_CASH", value : 3, year : 2014 }}
+{ buyer : "SHAW", seller : "PUIG", transaction: { tran_type : "MLB_DRAFT_CASH", value : 8, "year" : 2014 }},
+{ buyer : "PUIG", seller : "MAD", transaction: { tran_type : "MLB_DRAFT_CASH", value : 5, "year" : 2014 }},
+{ buyer : "PUIG", seller : "LAZ", transaction: { tran_type : "MLB_DRAFT_CASH", value : 8, "year" : 2014 }},
+{ buyer : "HIV+", seller : "SHAW", transaction: { tran_type : "MLB_DRAFT_CASH", value : 3, "year" : 2014 }},
+{ buyer : "PUIG", seller : "DBAG", transaction: { tran_type : "MLB_DRAFT_CASH", value : 3, "year" : 2014 }},
+{ buyer : "PUIG", seller : "MAD", transaction: { tran_type : "MLB_DRAFT_CASH", value : 7, "year" : 2014 }},
+{ buyer : "DBAG", seller : "PUIG", transaction: { tran_type : "FA_AUCTION_CASH", value : 1, "year" : 2014 }},
+{ buyer : "LAZ", seller : "PUIG", transaction: { tran_type : "MLB_DRAFT_CASH", value : 1, "year" : 2014 }},
+{ buyer : "CHOB", seller : "PUIG", transaction: { tran_type : "MLB_DRAFT_CASH", value : 3, "year" : 2014 }},
+{ buyer : "CHOB", seller : "PUIG", transaction: { tran_type : "FA_AUCTION_CASH", value : 99, "year" : 2014 }},
+{ buyer : "CHOB", seller : "PUIG", transaction: { tran_type : "MILB_DRAFT_SWAP_RIGHTS", value : 1, "year" : 2014 }},
+{ buyer : "SIDO", seller : "GOB", transaction: { tran_type : "MLB_DRAFT_CASH", value : 6, "year" : 2014 }},
+{ buyer : "GRAN", seller : "PUIG", transaction: { tran_type : "MLB_DRAFT_CASH", value : 10, "year" : 2014 }},
+{ buyer : "MAD", seller : "PUIG", transaction: { tran_type : "MLB_DRAFT_CASH", value : 13, "year" : 2014 }},
+{ buyer : "PUIG", seller : "SHAW", transaction: { tran_type : "MLB_DRAFT_CASH", value : 8, "year" : 2014 }},
+{ buyer : "SHAW", seller : "GLRY", transaction: { tran_type : "MLB_DRAFT_CASH", value : 5, "year" : 2014 }},
+{ buyer : "HIV+", seller : "PUIG", transaction: { tran_type : "MILB_DRAFT_SWAP_RIGHTS", value : 4, "year" : 2014 }},
+{ buyer : "GLRY", seller : "GOB", transaction: { tran_type : "MLB_DRAFT_CASH", value : 2, "year" : 2014 }},
+{ buyer : "PUIG", seller : "GRAN", transaction: { tran_type : "MLB_DRAFT_CASH", value : 7, "year" : 2014 }},
+{ buyer : "SHAW", seller : "GRAN", transaction: { tran_type : "MLB_DRAFT_CASH", value : 3, "year" : 2014 }},
+{ buyer : "SHAW", seller : "PUIG", transaction: { tran_type : "MLB_DRAFT_CASH", value : 3, "year" : 2014 }},
+{ buyer : "SHAW", seller : "PUIG", transaction: { tran_type : "MLB_DRAFT_CASH", value : 3, "year" : 2015 }},
+{ buyer : "PUIG", seller : "MAD", transaction: { tran_type : "MLB_DRAFT_CASH", value : 5, "year" : 2014 }},
+{ buyer : "GLRY", seller : "GOB", transaction: { tran_type : "MLB_DRAFT_CASH", value : 3, "year" : 2014 }},
+{ buyer : "SHAW", seller : "GLRY", transaction: { tran_type : "MLB_DRAFT_CASH", value : 5, "year" : 2014 }},
+{ buyer : "JEFF", seller : "MAD", transaction: { tran_type : "MLB_DRAFT_CASH", value : 5, "year" : 2014 }},
+{ buyer : "PUIG", seller : "JEFF", transaction: { tran_type : "MLB_DRAFT_CASH", value : 5, "year" : 2014 }},
+{ buyer : "SIDO", seller : "PUIG", transaction: { tran_type : "MLB_DRAFT_CASH", value : 3, "year" : 2014 }},
+{ buyer : "CHOB", seller : "GOB", transaction: { tran_type : "MILB_DRAFT_SWAP_RIGHTS", value : 3, "year" : 2014 }},
+{ buyer : "JEFF", seller : "CHOB", transaction: { tran_type : "MLB_DRAFT_CASH", value : 6, "year" : 2014 }},
+{ buyer : "MAD", seller : "CHOB", transaction: { tran_type : "MLB_DRAFT_CASH", value : 3, "year" : 2014 }}
 ];
 
 for (var i = 0; i < lastYearTrans.length; i++) {
@@ -85,7 +85,7 @@ for (var i = 0; i < lastYearTrans.length; i++) {
 	var finTran = new FinancialTransaction();
 	finTran.buyer = lastYearTrans[i].buyer;
 	finTran.seller = lastYearTrans[i].seller;
-	finTran.transaction = { type : lastYearTrans[i].transaction.type, value: lastYearTrans[i].transaction.value, year : lastYearTrans[i].transaction.year};
+	finTran.transaction = { tran_type : lastYearTrans[i].transaction.tran_type, value: lastYearTrans[i].transaction.value, year : lastYearTrans[i].transaction.year};
 	finTran.save();
 }
 console.log(count);
--- models/asset.js
+++ models/asset.js
@@ -4,7 +4,6 @@ var assetSchema = new mongoose.Schema({
 	type: String,
 	year: Number,
 	value: Number,
-	dollar_amount: Number,
 	originator: String,
 	current_owner: String	
 }, { collection: 'assets'});
@@ -18,29 +17,3 @@ assetSchema.statics.findForTeam = function(req, res, next) {
 
 var Asset = mongoose.model('assets', assetSchema);
 module.exports = Asset;
-
-/*
-	type: MILB Draft
-	year: 2014
-	draft_round: 5
-	originator: GOB
-	current_owner: JEFF
-
-	type: MLB AUCTION $
-	year: 2014
-	dollar_amount: 5
-	originator: GOB
-	current_owner: JEFF
-
-	type: FA AUCTION $
-	year: 2014
-	dollar_amount :20
-	originator: GOB
-	current_owner: JEFF
-
-	type: MILB DRAFT SWAP RIGHTS
-	year: 2014
-	draft_round: 3
-	originator: GOB
-	current_owner: JEFF
-*/
